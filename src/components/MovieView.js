c93a46af295257a78f2f3e781b1bdb0150cf7247
ncmoliver <mrmarquesoliver@gmail.com>
2025-06-25 00:04:08 -0400

    based project complted

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -24,9 +24,25 @@ const MovieView = () => {
         return <Hero text="Loading..." />
       }
       if(movieDetails) {
+        const posterPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+        const backdropUrl = `https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`
         return (
           <>
-            <Hero text={movieDetails.original_title}/>
+            <Hero text={movieDetails.original_title} backdrop={backdropUrl} />
+            <div className='container my-5'>
+              <div className='row'>
+                <div className='col-md-3'>
+                  <img src={posterPath} alt='...' className='img-fluid shadow rounded' />
+
+                </div>
+                <div className='col-md-9'>
+                  <h2>{movieDetails.original_title}</h2>
+                  <p className='col-md-9'> 
+                    {movieDetails.overview}
+                  </p>
+                </div>
+              </div>
+            </div>
           </>
         )
       }
